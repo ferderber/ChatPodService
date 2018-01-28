4286aaaae8ab876f26dec77dc8a83b13dda4d00f
Matthew Ferderber <matthewferderber@gmail.com>
2018-01-27 19:58:29 -0500

    send additional message data to user

--- MessageHandler.js
+++ MessageHandler.js
@@ -1,18 +1,23 @@
 const WebSocket = require('ws');
 
-function createMessageResponse() {
-  return {};
+function formatMessageResponse(message, user) {
+  return JSON.stringify({
+    message,
+    user: {
+      id: user.client.id,
+      name: user.name
+    },
+    send_date: new Date()
+  });
 }
 
 module.exports = function handleMessage(message, user, server) {
-  if (isCommand(message)) {
-    console.log('is a command');
-  } else {
+  if (isCommand(message)) {} else {
     server
       .clients
       .forEach((client) => {
         if (client !== user.client && client.readyState === WebSocket.OPEN) {
-          client.send(message);
+          client.send(formatMessageResponse(message, user));
         }
       });
   }
